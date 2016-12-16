d78892b5bf92025dde48b759b7a4d946e80adc8e
Martin Mikan <Martin.Mikan@email.cz>
2016-12-16 11:16:54 +0100

    Adding EntityInterface to basePaySystemVendor

--- Entity/BasePaySystem.php
+++ Entity/BasePaySystem.php
@@ -7,7 +7,6 @@ use Doctrine\ORM\PersistentCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 use Trinity\Component\Core\Interfaces\EntityInterface;
 use Knp\DoctrineBehaviors\Model as ORMBehaviors;
-use Trinity\Component\Core\Interfaces\EntityInterface;
 
 /**
  * PaySystem.
